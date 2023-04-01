b0ba032048b68ff11f8dec4cfa6b1a911dbcd338
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2023-04-01 18:02:35 +0200

    modelo de usuario implementado

--- Stitch/StitchStore/webserviceapp/models.py
+++ Stitch/StitchStore/webserviceapp/models.py
@@ -6,3 +6,14 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+
+
+class Usuarios(models.Model):
+    nombre = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
+    rol = models.IntegerField()
+    email = models.CharField(max_length=250)
+
+    class Meta:
+        managed = False
+        db_table = 'usuarios'
