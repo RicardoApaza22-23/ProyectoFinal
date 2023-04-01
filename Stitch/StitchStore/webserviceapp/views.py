118f70bda9511dc9fc205b5c2a2f1b054b604d21
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2023-04-01 14:32:56 +0200

    trabajando en la validación de el login

--- Stitch/StitchStore/webserviceapp/views.py
+++ Stitch/StitchStore/webserviceapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+import json
+from django.http import JsonResponse
 # Create your views here.
-def asda():
-    return "pok"
+def login(request):
+    return render(request,"login.html")
+def loginPOST(request):
+    requestForm = request.POST.dict()
+    nickname = requestForm.get('nickname')
+    return JsonResponse({"status" : nickname})
+    
