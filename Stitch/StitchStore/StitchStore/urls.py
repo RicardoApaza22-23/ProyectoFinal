497a2eb8b57fa8c42bb1c9bf2926f574297b475d
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2023-04-28 21:18:04 +0200

    url de administrador añadida

--- Stitch/StitchStore/StitchStore/urls.py
+++ Stitch/StitchStore/StitchStore/urls.py
@@ -29,6 +29,11 @@ urlpatterns = [
     path('usuario/perfil/',views.perfilForm),
     path('usuario/perfil/PerfilFormPost/', views.perfilFormPost),
     path('usuario/perfil/', views.perfilFormPost),
+<<<<<<< HEAD
     path('adminController/',views.administradorOperaciones)
+=======
+    path('adminController/',views.administradorOperaciones),
+    path('adminController/editar/<int:id_usuario_conectado>', views.adminEditarUsuario)
+>>>>>>> main
     
 ]
