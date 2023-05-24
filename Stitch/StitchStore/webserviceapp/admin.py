919dc50ad94f867531325f32c7f2cc38c010df27
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2023-05-24 16:38:50 +0200

    creado nuevo superadmin

--- Stitch/StitchStore/webserviceapp/admin.py
+++ Stitch/StitchStore/webserviceapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Usuarios, Producto, Perfil
+admin.site.register(Usuarios)
+admin.site.register(Producto)
+admin.site.register(Perfil)
 # Register your models here.
