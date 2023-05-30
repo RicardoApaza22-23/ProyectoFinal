93093185eef91531e3de3b9e3a2d5a2124b7b041
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2023-05-30 11:29:53 +0200

    models añadidos para la el superadmin

--- Stitch/StitchStore/webserviceapp/admin.py
+++ Stitch/StitchStore/webserviceapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Usuarios, Producto, Perfil
+from .models import Usuarios, Producto, Perfil,Carrito
 admin.site.register(Usuarios)
 admin.site.register(Producto)
 admin.site.register(Perfil)
+admin.site.register(Carrito)
 # Register your models here.
