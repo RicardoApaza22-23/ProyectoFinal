cac0ab829391eb94b9b4ce8b494258649ae5ee9d
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2023-06-01 09:46:58 +0200

    integrados nuevos modelos en administración

--- Stitch/StitchStore/webserviceapp/admin.py
+++ Stitch/StitchStore/webserviceapp/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Usuarios, Producto, Perfil,Carrito
+from .models import Usuarios, Producto, Perfil,Carrito,Estacion,EstacionProducto,Etiqueta,ProductoEtiqueta,Favoritos
+# Register your models here.
 admin.site.register(Usuarios)
 admin.site.register(Producto)
 admin.site.register(Perfil)
 admin.site.register(Carrito)
-# Register your models here.
+admin.site.register(Estacion)
+admin.site.register(EstacionProducto)
+admin.site.register(Etiqueta)
+admin.site.register(ProductoEtiqueta)
+admin.site.register(Favoritos)
